6f938b0d46714866cce79f86cd9c38a561172e97
Maxi Saparov <maxi.saparov@gmail.com>
2024-12-10 14:17:43 -0500

    added: tuigreet

---
+++ modules/greeter/tuigreet/default.nix
@@ -0,0 +1,34 @@
+{pkgs, specialArgs, ...}: {
+  services = {
+    xserver.enable = false;
+
+    greetd = {
+      enable = true;
+
+      settings = {
+	default_session = {
+	  user = specialArgs.username;
+	  command = "${pkgs.greetd.tuigreet}/bin/tuigreet
+	    -t 
+	    --theme 'border=magenta;text=cyan;prompt=cyan'
+	    --cmd $HOME/.wayland-session";
+	};
+      };
+    };
+  };
+
+  # this is a life saver.
+  # literally no documentation about this anywhere.
+  # might be good to write about this...
+  # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
+}

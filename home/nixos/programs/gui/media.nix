974647c925d9d65c9b705d1facea45a9ecfd4cf7
Maxi Saparov <maxi.saparov@gmail.com>
2026-04-22 16:12:05 -0400

    fix(gui): actually use gui option

--- home/nixos/programs/gui/media.nix
+++ home/nixos/programs/gui/media.nix
@@ -1,50 +1,57 @@
 {
   pkgs,
+  lib,
+  config,
   inputs,
   ...
 }:
+with lib;
 # Media - control and enjoy audio/video
 {
   imports = [
     inputs.spicetify.homeManagerModules.default
   ];
 
-  # Media packages
-  home.packages = with pkgs; [
-    # utils
-    ffmpeg-full
-    playerctl
-    pavucontrol
-    imv
-    grim # wayland screenshots - needed for satty
-    satty # screenshot annotator
+  config = mkIf config.programs.gui.enable {
 
-    # misc
-    cava
-    krita
-  ];
+    # Media packages
+    home.packages = with pkgs; [
+      # utils
+      ffmpeg-full
+      playerctl
+      pavucontrol
+      imv
+      grim # wayland screenshots - needed for satty
+      satty # screenshot annotator
 
-  # Media programs we want to manage w/ HM
-  programs = {
-    obs-studio = {
-      enable = true;
+      # misc
+      cava
+      krita
+    ];
 
-      plugins = with pkgs.obs-studio-plugins; [
-        wlrobs
-        obs-vaapi
-        obs-backgroundremoval
-        obs-pipewire-audio-capture
-      ];
+    # Media programs we want to manage w/ HM
+    programs = {
+      obs-studio = {
+        enable = true;
+
+        plugins = with pkgs.obs-studio-plugins; [
+          wlrobs
+          obs-vaapi
+          obs-backgroundremoval
+          obs-pipewire-audio-capture
+        ];
+      };
+
+      # pretty spotify managed by stylix
+      spicetify = lib.mkIf (pkgs.stdenv.hostPlatform.system != "aarch64-linux") {
+        enable = true;
+      };
     };
 
-    # pretty spotify managed by stylix
-    spicetify = {
-      enable = true;
+    # playback control
+    services = {
+      playerctld.enable = true;
     };
-  };
 
-  # playback control
-  services = {
-    playerctld.enable = true;
   };
 }
